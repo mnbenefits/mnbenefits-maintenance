c921a8f4b88ee001720c3fbb61d8d3c40c85f4b1
EricMN35Johnson <eric.m.johnson@state.mn.us>
2024-09-06 11:06:31 -0500

    Initial commit

---
+++ src/main/java/state/mn/us/MnbenefitsMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package state.mn.us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnbenefitsMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnbenefitsMaintenanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/state/mn/us/MaintenanceTests.java
@@ -0,0 +1,13 @@
+package state.mn.us;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
