33ba8738dbadf05b8cf1b2a0e8b8bbcfa0319647
EricMN35Johnson <eric.m.johnson@state.mn.us>
2024-09-25 13:27:33 -0500

    Added /pages to controller mapping

--- src/main/java/state/mn/us/MaintenancePageController.java
+++ src/main/java/state/mn/us/MaintenancePageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MaintenancePageController {
 
-	@GetMapping({"/","/*/*"})
+	@GetMapping({"/","/*/*", "/pages/*"})
 	public String showHomePage() {
 	    return "/maintenance.html";   
 	   }
