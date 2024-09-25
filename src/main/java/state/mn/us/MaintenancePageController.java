7689154a595ff17593c639e0cd1189bac83dcedf
EricMN35Johnson <eric.m.johnson@state.mn.us>
2024-09-25 14:17:34 -0500

    Added post mapping

--- src/main/java/state/mn/us/MaintenancePageController.java
+++ src/main/java/state/mn/us/MaintenancePageController.java
@@ -2,14 +2,21 @@ package state.mn.us;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MaintenancePageController {
 
 	@GetMapping({"/","/*/*", "/pages/*"})
 	public String showHomePage() {
+		System.out.println("GET!");
 	    return "/maintenance.html";   
 	   }
+	@PostMapping({"/","/*/*", "/pages/*"})
+	public String postMapping() {
+		System.out.println("POST!");
+		return "/maintenance.html";
+	}
 	
 	
 }
