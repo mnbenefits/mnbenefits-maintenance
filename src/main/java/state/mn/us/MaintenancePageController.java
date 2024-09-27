8d730d19ff063048c68742048d9a8f855efeac1c
EricMN35Johnson <eric.m.johnson@state.mn.us>
2024-09-27 08:24:37 -0500

    Added more images, changed controller to load images.

--- src/main/java/state/mn/us/MaintenancePageController.java
+++ src/main/java/state/mn/us/MaintenancePageController.java
@@ -1,23 +1,59 @@
 package state.mn.us;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @Controller
 public class MaintenancePageController {
 
-	@GetMapping({"/","/*/*", "/pages/*"})
-	public String showHomePage() {
+	@GetMapping({"/", "/pages/*"})
+	public String showMaintenancePage() {
 		System.out.println("GET!");
 	    return "/maintenance.html";   
 	   }
-	@RequestMapping(value={"/","/*/*", "/pages/*"} , method = RequestMethod.POST)
+	
+//	@RequestMapping(value={"/","/*/*", "/pages/*"} , method = RequestMethod.POST)
+//	public String postMapping() {
+//		System.out.println("POST!");
+//		return "/maintenance.html";
+//	}
+	
+	@PostMapping(value={"/","/*/*", "/pages/*"}  )
 	public String postMapping() {
 		System.out.println("POST!");
 		return "/maintenance.html";
+	}
+	
+
+	
+	@GetMapping(value = "/images/{fileName}",  produces = MediaType.TEXT_PLAIN_VALUE)
+	@ResponseBody
+	public ResponseEntity<byte[]> imageMapping(@PathVariable String fileName) throws IOException {
+        // Get the image from your storage (e.g., file system, database)
+        File imageFile = new File("src/main/resources/static/images/" + fileName);
+        byte[] imageBytes = Files.readAllBytes(imageFile.toPath());
+
+        // Set the content type based on the image format
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG); 
+
+        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
 	}
 	
 	
--- src/main/java/state/mn/us/MvcConfig.java
+++ src/main/java/state/mn/us/MvcConfig.java
@@ -1,13 +1,14 @@
 package state.mn.us;
 
+import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class MvcConfig implements WebMvcConfigurer {
+public class MvcConfig extends DelegatingWebMvcConfiguration /* implements WebMvcConfigurer */ {
 
-	 @Override
-	   public void addResourceHandlers(ResourceHandlerRegistry registry) {  
-	    registry.addResourceHandler("/images/**").addResourceLocations("/images/");
-	    registry.addResourceHandler("/fonts/**").addResourceLocations("/fonts/");
-	   }
+//	 @Override
+//	   public void addResourceHandlers(ResourceHandlerRegistry registry) {  
+//	    registry.addResourceHandler("/images/**").addResourceLocations("classpath:/images/");
+//	    registry.addResourceHandler("/fonts/**").addResourceLocations("/fonts/");
+//	   }
 }
