3133279643cbf284f55c8fecb9831d11dd7599d1
EricMN35Johnson <eric.m.johnson@state.mn.us>
2024-09-27 11:59:46 -0500

    Troubleshooting image file path.

--- src/main/java/state/mn/us/MaintenancePageController.java
+++ src/main/java/state/mn/us/MaintenancePageController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class MaintenancePageController {
@@ -44,14 +46,36 @@ public class MaintenancePageController {
 	
 	@GetMapping(value = "/images/{fileName}",  produces = MediaType.TEXT_PLAIN_VALUE)
 	@ResponseBody
-	public ResponseEntity<byte[]> imageMapping(@PathVariable String fileName) throws IOException {
+	public ResponseEntity<byte[]> getImage(HttpServletRequest request, @PathVariable String fileName) throws IOException {
+		String rpath = request.getContextPath();
+		System.out.println("====== getImage for " + fileName);
+		System.out.println("rpath = " + rpath);
         // Get the image from your storage (e.g., file system, database)
         File imageFile = new File("src/main/resources/static/images/" + fileName);
+        System.out.println("====== File exists = " + imageFile.exists());
         byte[] imageBytes = Files.readAllBytes(imageFile.toPath());
 
         // Set the content type based on the image format
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.IMAGE_PNG); 
+
+        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/documents/{fileName}",  produces = MediaType.TEXT_PLAIN_VALUE)
+	@ResponseBody
+	public ResponseEntity<byte[]> getDocuments(HttpServletRequest request, @PathVariable String fileName) throws IOException {
+		String rpath = request.getContextPath();
+		System.out.println("====== getDocuments for " + fileName);
+		System.out.println("rpath = " + rpath);
+        // Get the image from your storage (e.g., file system, database)
+        File imageFile = new File("src/main/resources/static/documents/" + fileName);
+        System.out.println("====== File exists = " + imageFile.exists());
+        byte[] imageBytes = Files.readAllBytes(imageFile.toPath());
+
+        // Set the content type based on the image format
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF); 
 
         return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
 	}
--- src/main/java/state/mn/us/MvcConfig.java
+++ src/main/java/state/mn/us/MvcConfig.java
@@ -1,14 +1,24 @@
 package state.mn.us;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class MvcConfig extends DelegatingWebMvcConfiguration /* implements WebMvcConfigurer */ {
+public class MvcConfig /* extends DelegatingWebMvcConfiguration */  implements WebMvcConfigurer  {
 
 //	 @Override
 //	   public void addResourceHandlers(ResourceHandlerRegistry registry) {  
 //	    registry.addResourceHandler("/images/**").addResourceLocations("classpath:/images/");
-//	    registry.addResourceHandler("/fonts/**").addResourceLocations("/fonts/");
+//	    registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/fonts/");
+//	    registry.addResourceHandler("/documents/**").addResourceLocations("classpath:/documents/");
 //	   }
+//	
+//	@Override
+//	public void configurePathMatch(PathMatchConfigurer configurer) {
+//		configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(Controller.class));
+//	}
 }
