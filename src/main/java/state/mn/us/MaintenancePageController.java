d88f01db1cc8399668ddf56be0833d7085416cf9
EricMN35Johnson <eric.m.johnson@state.mn.us>
2024-10-02 07:37:04 -0500

    Edited to basic page, removed faq and other pages.

--- src/main/java/state/mn/us/MaintenancePageController.java
+++ src/main/java/state/mn/us/MaintenancePageController.java
@@ -12,17 +12,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class MaintenancePageController {
+	
+	@GetMapping({"/pages/healthcareRenewalUpload"})
+	public String showHealthCareUploadMaintenancePage() {
+		System.out.println("GET HC!");
+	    return "/hcmaintenance.html";   
+	   }
 
 	@GetMapping({"/", "/pages/*", "/home", "/error"})
 	public String showMaintenancePage() {
